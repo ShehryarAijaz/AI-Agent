431f4b105b3876d160a8f26562169a2583d8834b
Shehryar Aijaz <shehryar120345@gmail.com>
2025-01-18 07:54:02 +0500

    feat: added profile route

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import * as userController from '../controllers/user.controller.js';
 import { body } from 'express-validator';
+import * as authMiddleware from '../middleware/auth.middleware.js';
 
 const router = Router();
 
@@ -8,5 +9,12 @@ router.post('/register',
     body('email').isEmail().withMessage('Email must be a valid email'),
     body('password').isLength({ min: 3 }).withMessage('Password must be atleast 3 characters long'),
     userController.createUserController)
+
+router.post('/login',
+    body('email').isEmail().withMessage('Email must be a valid email'),
+    body('password').isLength({ min: 3 }).withMessage('Password must be atleast 3 characters long'),
+    userController.loginController);
+
+router.get('/profile', authMiddleware.authUser, userController.profileController);
 
 export default router; 
