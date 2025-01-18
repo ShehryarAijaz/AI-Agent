a1e9465d5602d352c4c09a447954597a664d1d69
Shehryar Aijaz <shehryar120345@gmail.com>
2025-01-18 07:52:36 +0500

    feat: added profileController functionality

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -20,3 +20,45 @@ export const createUserController = async (req, res) => {
         res.status(400).json({ message: errors.message || 'User creation failed' });
     }   
 }
+
+export const loginController = async (req, res) => {
+
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+
+        return res.status(400).json({ errors: errors.array() });
+        }
+        try {
+
+            const { email, password } = req.body;
+
+            const user = await userModel.findOne({ email }).select('+password');
+
+            if (!user) {
+                return res.status(401).json({ message:errors.message || 'Invalid email' });
+            }
+
+            const isValid = await user.isValidPassword(password);
+
+            if (!isValid) {
+                return res.status(401).json({ message:errors.message || 'Invalid password' })
+            }
+
+            const token = await user.generateJWT();
+
+            res.status(200).json({ user, token })
+
+            } catch (errors) {
+
+                console.log(errors); // Log the actual error
+
+                res.status(400).json({ message: errors.message || 'Login failed' });
+                }
+}
+
+export const profileController = async (req, res) => {    
+    res.status(200).json({
+        user: req.user,
+    });
+}
