dc2cc227b9628ea6af46a513a8b609c4766ea85e
Shehryar Aijaz <shehryar120345@gmail.com>
2025-01-18 06:38:55 +0500

    feat: initial commit

---
+++ backend/controllers/user.controller.js
@@ -0,0 +1,22 @@
+import userModel from '../models/user.model.js';
+import * as userService from '../services/user.service.js'
+import { validationResult } from 'express-validator';
+
+export const createUserController = async (req, res) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+
+    try {
+        const user = await userService.createUser(req.body);
+
+        const token = await user.generateJWT();
+
+        res.status(201).json({ user, token });
+    } catch (errors) {
+        console.error(errors); // Log the actual error
+        res.status(400).json({ message: errors.message || 'User creation failed' });
+    }    
+}
