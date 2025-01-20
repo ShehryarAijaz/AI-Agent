93f46f49a2d6954b93e467d16a8310ef113800a7
Shehryar Aijaz <shehryar120345@gmail.com>
2025-01-20 05:30:00 +0500

    feat: added logout functionality

--- backend/middleware/auth.middleware.js
+++ backend/middleware/auth.middleware.js
@@ -1,10 +1,20 @@
 import jwt from 'jsonwebtoken';
+import redisClient from '../services/redis.service.js';
 
 export const authUser = async (req, res, next) => {
     try {
         const token = req.cookies.token || req.headers.authorization.split(' ')[1];
 
         if (!token) {
+            return res.status(401).json({ message: 'Unauthorized User' });
+        }
+
+        const isBlackListed = await redisClient.get(token);
+
+        if (isBlackListed) {
+
+            res.cookie('token', '');
+
             return res.status(401).json({ message: 'Unauthorized User' });
         }
 
@@ -15,7 +25,6 @@ export const authUser = async (req, res, next) => {
 
             console.log(error);
             
-
             res.status(401).send({ message: 'Please authenticate.' });
             }
 }
