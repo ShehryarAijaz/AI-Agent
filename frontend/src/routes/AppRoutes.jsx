c4f10baf7d5357271e8c2a50de95ae66a86a3176
Shehryar Aijaz <shehryar120345@gmail.com>
2025-02-09 06:26:41 +0500

    feat: login, register ui & functionalities

---
+++ frontend/src/routes/AppRoutes.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Route, BrowserRouter, Routes } from 'react-router-dom'
+import Login from '../screens/Login.jsx'
+import Register from '../screens/Register.jsx'
+import Home from '../screens/Home.jsx'
+
+const AppRoute = () => {
+    return (
+        <BrowserRouter>
+        <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+        </Routes>
+        </BrowserRouter>
+    )}
+
+export default AppRoute;
